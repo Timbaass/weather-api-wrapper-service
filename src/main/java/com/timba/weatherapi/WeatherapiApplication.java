6b15154f96f1fa4aa7eadf2aa3da9dec9c4e507a
Timbbass <msozgur44@gmail.com>
2026-03-09 09:47:36 +0300

    redis configuration added.

--- src/main/java/com/timba/weatherapi/WeatherapiApplication.java
+++ src/main/java/com/timba/weatherapi/WeatherapiApplication.java
@@ -4,8 +4,10 @@ import com.timba.weatherapi.config.WeatherConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WeatherapiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/timba/weatherapi/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.timba.weatherapi.config;
+
+import com.timba.weatherapi.domains.WeatherResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(15))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new Jackson2JsonRedisSerializer<>(WeatherResponse.class)));
+
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+}
