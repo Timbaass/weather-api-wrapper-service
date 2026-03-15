bb21637497cbca387ac0751d8898947a0ee5be0d
Timbbass <msozgur44@gmail.com>
2026-03-15 15:51:42 +0300

    validation added for getWeather endpoint and general exception handler added.

--- src/main/java/com/timba/weatherapi/config/GlobalExceptionHandler.java
+++ src/main/java/com/timba/weatherapi/config/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package com.timba.weatherapi.config;
 
 import com.timba.weatherapi.exception.CityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.timba.weatherapi.domains.ErrorResponse;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +17,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleCityNotFoundException(CityNotFoundException ex){
         ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex){
+        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/timba/weatherapi/controller/WeatherController.java
+++ src/main/java/com/timba/weatherapi/controller/WeatherController.java
@@ -2,12 +2,10 @@ package com.timba.weatherapi.controller;
 
 import com.timba.weatherapi.domains.WeatherResponse;
 import com.timba.weatherapi.service.IWeatherService;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/weather")
@@ -17,7 +15,7 @@ public class WeatherController {
     private final IWeatherService weatherService;
 
     @GetMapping("/{city}")
-    public ResponseEntity<WeatherResponse> getWeather(@PathVariable String city) {
+    public ResponseEntity<WeatherResponse> getWeather(@PathVariable @NotBlank(message = "City name can not be empty.") String city) {
         WeatherResponse response = weatherService.getWeatherData(city);
         return ResponseEntity.ok(response);
     }
